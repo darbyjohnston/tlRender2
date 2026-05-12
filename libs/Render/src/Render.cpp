855dbe7d98f19062b523b1026fc1889298c17df8
Darby Johnston <darbyjohnston@yahoo.com>
2026-05-12 16:22:36 -0700

    Refactoring

--- libs/Render/src/Render.cpp
+++ libs/Render/src/Render.cpp
@@ -156,6 +156,14 @@ namespace tl
             for (const auto& input : inputs)
             {
                 OIIO::ImageBuf wrapped = wrap(*input);
+                if (3 == wrapped.spec().nchannels)
+                {
+                    // Add an alpha channel.
+                    const int channelOrder[] = { 0, 1, 2, -1 };
+                    const float channelValues[] = { 0, 0, 0, 1.0 };
+                    const std::string channelNames[] = { "", "", "", "A" };
+                    wrapped = OIIO::ImageBufAlgo::channels(wrapped, 4, channelOrder, channelValues, channelNames);
+                }
                 OIIO::ImageBuf p;
                 if (!OIIO::ImageBufAlgo::premult(p, wrapped))
                 {
